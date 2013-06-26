49f3851bc0e8363bf6b4ba304bfc962615d5bced
Yan <yan@mit.edu>
2013-06-26 15:16:31 -0700

    Filter out non-Firefox http requests

--- sniffer.py
+++ sniffer.py
@@ -3,7 +3,10 @@
 import subprocess
 
 def sniffedUrls(interface="wlan0"):
-    p = subprocess.Popen(["tshark", "-i", interface, "-p", "port", "80", "-T", "fields", "-e", "http.request.method", "-e", "http.request.full_uri"], stdout=subprocess.PIPE)
+    p = subprocess.Popen(["tshark", "-i", interface, "-p", "port", "80", "-T", "fields", "-e", "http.request.method", "-e", "http.request.full_uri", "-e", "http.user_agent"], stdout=subprocess.PIPE)
     while True:
         line = p.stdout.readline().strip()
-        if line: yield line.split()[1]
+        if line:
+            agent = line.split()[2:]
+            if any(map(lambda x: 'Firefox' in x, agent)):
+                yield line.split()[1]
